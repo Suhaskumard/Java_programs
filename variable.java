fed9cb54b7d9eed3c1c3c3ce2354c86efbf2485a
Suhas Kumar D <suhaskumard4@gmail.com>
2025-03-22 10:17:40 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,17 @@
+public class variable {
+        public static void main(String[] args)
+        {
+         var x = 100;
+            var y = 1.90;
+            var z = 'a';
+            var p = "tanu";
+            var q = false;
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(z);
+            System.out.println(p);
+            System.out.println(q);
+        }
+    }
+    
+
