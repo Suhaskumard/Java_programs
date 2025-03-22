aad87c31a988c17e985cb90fd76676f8cdd929a4
Suhas Kumar D <suhaskumard4@gmail.com>
2025-03-22 10:07:50 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,7 @@
+public class ternary {
+    public static void main(String args[]) {
+    int time=20;
+    String result = (time < 18) ? "Godd day." : "Good evening.";
+    System.out.println(result);
+    }
+}
