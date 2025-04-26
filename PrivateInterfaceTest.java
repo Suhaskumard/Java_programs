c3181f6ab1e75b50518b948bed130f246ef1d43a
Suhas Kumar D <suhaskumard4@gmail.com>
2025-04-26 11:35:49 +0530

    Add files via upload

---
+++ PrivateInterfaceTest.java
@@ -0,0 +1,24 @@
+interface Greeting {
+    private void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+
+    default void sayHello() {
+        log("Hello");
+    }
+
+    default void sayGoodbye() {
+        log("Goodbye");
+    }
+}
+
+class Greeter implements Greeting {
+}
+
+public class PrivateInterfaceTest {
+    public static void main(String[] args) {
+        Greeter g = new Greeter();
+        g.sayHello();
+        g.sayGoodbye();
+    }
+}
