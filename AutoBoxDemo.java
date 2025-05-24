d8822a0a71f32b9389b9a87ad3cc9bfa6887607d
Suhas Kumar D <suhaskumard4@gmail.com>
2025-05-24 11:30:49 +0530

    Add files via upload

---
+++ AutoBoxDemo.java
@@ -0,0 +1,8 @@
+public class AutoBoxDemo {
+    public static void main(String[] args) {
+        Integer i0b =100;
+        int i = i0b;
+        System.out.println( i + " " + i0b);
+    }
+    
+}
